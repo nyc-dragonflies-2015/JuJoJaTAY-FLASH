5c706f78da4cebb12979c4c15ae466ea0bf7f9fd
johnlyden <john.lyden6@gmail.com>
2015-06-12 18:51:49 -0400

    functioning answer checker and session guesses iterates through next question

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -1,10 +1,22 @@
 get '/decks' do
-  #index of all flash card decks
+  @decks = Deck.all
   erb :'/decks/index'
 end
 
 get '/decks/:id' do
-  #find the deck through id
-  #
+  @deck = Deck.find_by(id: params[:id])
+  session[:guesses] = 0
+  erb :'/decks/show'
 
+end
+
+post '/decks/:id' do
+  @deck = Deck.find_by(id: params[:id])
+  if params[:answer] == @deck.cards[session[:guesses]].answer
+    "hayyy"
+  else
+    redirect '/decks'
+  end
+  session[:guesses] += 1
+  erb :'/decks/show'
 end
