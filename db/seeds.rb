0fcab976e5ddeecff86bab8543f8d899f1114865
johnlyden <john.lyden6@gmail.com>
2015-06-12 16:59:36 -0400

    successfully seeding databse with card objects associated to created Deck

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
-File.open('').each_line do |word|
-  Card.create(question: )
-end
+harry_deck = Deck.create(name: "Harry Potter Trivia", description: "Test your Harry Potter knowledge you dork")
+
+
+File.open('db/fixtures/harry.txt').each_slice(2) {|question, answer| Card.create(question: question.chomp, answer: answer.chomp).update_attributes(deck_id: harry_deck.id)}
